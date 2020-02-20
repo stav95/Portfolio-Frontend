0e806a0e47b72df33c7ffa03d7b38a366022d4be
stav95 <stav.dev95@gmail.com>
2020-02-20 23:10:36 +0200

    2

--- src/test.js
+++ src/test.js
@@ -1,27 +1,58 @@
 import React, { Component } from "react";
 import Axios from "axios";
+import "./todo-list/main.css";
 
 export default class Test extends Component {
   constructor() {
     super();
     this.state = {
-      tmp: "nothing yet"
+      list_name: "",
+      list_items: []
     };
   }
 
   componentDidMount = () => {
-    Axios.get("/test").then(response => {
-      console.log(response);
+    Axios.get("/todo-list").then(response => {
+      console.log(response.data);
       this.setState({
-        tmp: "completed"
+        list_name: response.data.name,
+        list_items: response.data.items
       });
     });
   };
 
   render() {
+    let list_items = this.state.list_items;
+    let list_name = this.state.list_name;
+
     return (
       <div>
-        <h1>The response is: {this.state.tmp}</h1>
+        <div className="box" id="heading">
+          <h1>{list_name}</h1>
+        </div>
+        <div className="box">
+          {list_items.map((value, idx) => (
+            <form action="/delete" method="post">
+              <div className="item">
+                <input type="checkbox" name="checkbox" value={value} />
+                <p>{value}</p>
+              </div>
+              <input type="hidden" name="listName" value={list_name}></input>
+            </form>
+          ))}
+
+          <form class="item" action="/" method="post">
+            <input
+              type="text"
+              name="newItem"
+              placeholder="New Item"
+              autocomplete="off"
+            />
+            <button type="submit" name="list" value={list_name}>
+              +
+            </button>
+          </form>
+        </div>
       </div>
     );
   }
